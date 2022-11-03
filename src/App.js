b42d8e263e2937b8559d4938c7a6f17519d401b4
Andersonvb <andersonvalenciabermudez@gmail.com>
2022-11-03 08:41:44 -0500

    Add grid layout

--- src/App.js
+++ src/App.js
@@ -5,44 +5,65 @@ import Card from "./components/Card.js";
 function App() {
   return (
     <main className="main-container">
-      <UserCard />
+      <div className="container">
+        <div className="item item-1">
+          <UserCard />
+        </div>
 
-      <Card
-        color="light-orange"
-        title="Work"
-        time="32hrs"
-        date="Last Week - 36hrs"
-      />
-      <Card
-        color="soft-blue"
-        title="Play"
-        time="10hrs"
-        date="Last Week - 8hrs"
-      />
-      <Card
-        color="light-red"
-        title="Study"
-        time="4hrs"
-        date="Last Week - 4hrs"
-      />
-      <Card
-        color="lime-green"
-        title="Exercise"
-        time="4hrs"
-        date="Last Week - 5hrs"
-      />
-      <Card
-        color="violet"
-        title="Social"
-        time="5hrs"
-        date="Last Week - 10hrs"
-      />
-      <Card
-        color="soft-orange"
-        title="Self Care"
-        time="2hrs"
-        date="Last Week - 2hrs"
-      />
+        <div className="item">
+          <Card
+            color="light-orange"
+            title="Work"
+            time="32hrs"
+            date="Last Week - 36hrs"
+          />
+        </div>
+
+        <div className="item">
+          <Card
+            color="soft-blue"
+            title="Play"
+            time="10hrs"
+            date="Last Week - 8hrs"
+          />
+        </div>
+
+        <div className="item">
+          <Card
+            color="light-red"
+            title="Study"
+            time="4hrs"
+            date="Last Week - 4hrs"
+          />
+        </div>
+
+        <div className="item">
+          <Card
+            color="lime-green"
+            title="Exercise"
+            time="4hrs"
+            date="Last Week - 5hrs"
+          />
+        </div>
+
+        <div className="item">
+          <Card
+            color="violet"
+            title="Social"
+            time="5hrs"
+            date="Last Week - 10hrs"
+          />
+        </div>
+
+        <div className="item">
+          <Card
+            color="soft-orange"
+            title="Self Care"
+            time="2hrs"
+            date="Last Week - 2hrs"
+          />
+        </div>
+      </div>
     </main>
   );
 }
